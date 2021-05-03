0d723779dc645fa4f57bbe2b60a303b8ec33452d
Nite <niteblock@gmail.com>
2021-05-03 12:03:57 +0200

    new fix

--- exts/logs.py
+++ exts/logs.py
@@ -10,7 +10,7 @@ class Logs(commands.Cog):
     @tasks.loop(seconds=60)
     async def update_log_channels(self):
         print("Saving log channels...")
-        await self.wait_until_ready()
+        await self.bot.wait_until_ready()
         self.messages = self.bot.get_channel(self.bot.config["logs"]["messages"])
         self.user = self.bot.get_channel(self.bot.config["logs"]["user"])
         self.punishment = self.bot.get_channel(self.bot.config["logs"]["punishment"])
